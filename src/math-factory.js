0d807a35120b994776e88003e4beabd1e74a9843
Yeison Gomez <yeigomez97@hotmail.com>
2016-12-05 12:50:07 -0500

    edit readme use

--- src/math-factory.js
+++ src/math-factory.js
@@ -6,7 +6,6 @@
     angular.module('ng-math-factory', [
             'math.methods',
             'math.general',
-<<<<<<< HEAD
             'math.adjust-curve'
         ])
         .factory('$math', ['$q', '$methods', 'adjustCurve', 'general', function($q, $methods, adjustCurve, general) {
@@ -14,47 +13,24 @@
                 resolve: resolve,
                 getMethods: getMethods
             };
-=======
-            'math.adjust-curve',
-            'math.search-raiz'
-        ])
-        .factory('$math', [
-            '$q', '$methods', 'adjustCurve', 'general', 'searchRaiz',
-            function($q, $methods, adjustCurve, general, searchRaiz){
-                return {
-                    resolve: resolve,
-                    getMethods: getMethods
-                };
->>>>>>> fde4456b8d7bba703e14ce4eead433f483b65871
 
-                function resolve(method, input, routeDep, callback) {
-                    var methods_factory = $methods;
-                    for (var i = 0; i < methods_factory.length; i++) {
-                        if (method.name === methods_factory[i].name) {
-                            if (methods_factory[i].libs !== undefined) {
-                                for (var k = 0; k < methods_factory[i].libs.length; k++) {
-                                    var oHead = document.getElementsByTagName('head')[0];
-                                    var oScript = document.createElement('script');
-                                    oScript.type = 'text/javascript';
-                                    oScript.charset = 'utf-8';
-                                    oScript.src = routeDep + routeLib + methods_factory[i].libs[k];
-                                    oHead.appendChild(oScript);
-                                }
-                            }
-                            eval(methods_factory[i].factory).options(input, method.sub).then(function(data) {
-                                var html = data[1];
-                                html.resolve = routeDep + routeLib + html.resolve;
-                                html.graphics = (html.graphics === undefined) ? undefined : routeDep + routeLib + html.graphics;
-                                callback(data[0], html);
-                            });
-                            break;
-                        }
+            function resolve(method, input, routeDep, callback) {
+                var methods_factory = $methods;
+                for (var i = 0; i < methods_factory.length; i++) {
+                    if (method.name === methods_factory[i].name) {
+                        eval(methods_factory[i].factory).options(input, method.sub).then(function(data) {
+                            var html = data[1];
+                            html.resolve = routeDep + routeLib + html.resolve;
+                            html.graphics = routeDep + routeLib + html.graphics;
+                            callback(data[0], html);
+                        });
+                        break;
                     }
                 }
-
-                function getMethods() {
-                    return $methods;
-                }
             }
-        ]);
+
+            function getMethods() {
+                return $methods;
+            }
+        }]);
 })();
--- src/methods.js
+++ src/methods.js
@@ -5,19 +5,6 @@
         var routeLib = "/ng-math-factory/src";
         return [{
             name: 'General',
-<<<<<<< HEAD
-=======
-            sub: [
-                { name: 'Operación basica', in : 'formula', readme: routeLib + '/general/readme/op_basic.html' },
-                { name: 'Derivar', in : 'formula', readme: routeLib + '/general/readme/derive.html' }
-            ],
-            factory: 'general',
-            libs: [
-                '/general/lib/derive.js'
-            ]
-        }, {
-            name: 'Ajuste de curvas',
->>>>>>> fde4456b8d7bba703e14ce4eead433f483b65871
             sub: [
                 { name: 'Operación basica', in : 'formula', readme: routeLib + '/general/readme/op_basic.html' }/*,
                 { name: 'Derivar', in : 'formula', readme: routeLib + '/general/readme/derive.html' }*/
@@ -27,7 +14,6 @@
                 '/general/lib/derive.js'
             ]*/
         }, {
-<<<<<<< HEAD
             name: 'Ajuste de curvas',
             sub: [
                 { name: 'Mínimos cuadrados', in : 'xy' }
@@ -36,10 +22,6 @@
         }, {
             name: 'Búsqueda de raíces',
             sub: [
-=======
-            name: 'Búsqueda de raíces',
-            sub: [
->>>>>>> fde4456b8d7bba703e14ce4eead433f483b65871
                 { name: 'Punto fijo', in : 'formula', readme: routeLib + '/search_raiz/readme/point_fixed.html' },
                 { name: 'Bisección', in : 'formula', readme: routeLib + '/search_raiz/readme/bisection.html' },
                 { name: 'Newton Raphson', in : 'formula', readme: routeLib + '/search_raiz/readme/newton.html' },
