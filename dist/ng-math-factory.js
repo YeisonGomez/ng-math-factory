efcaf0ca6db11a779cdcbfcc50be57b5aaef84c2
Yeison Gomez <yeigomez97@hotmail.com>
2016-12-07 00:11:00 -0500

    biseccion metodo bolzano

--- dist/ng-math-factory.js
+++ dist/ng-math-factory.js
@@ -32,6 +32,7 @@
                             }
                             eval(methods_factory[i].factory).options(input, method.sub).then(function(data) {
                                 var html = data[1];
+                                console.log(data);
                                 html.resolve = routeDep + routeLib + html.resolve;
                                 html.graphics = (html.graphics === undefined) ? undefined : routeDep + routeLib + html.graphics;
                                 callback(data[0], html);
@@ -81,6 +82,191 @@
             factory: 'searchRaiz'
         }];
     });
+})();
+
+(function() {
+    'use strict';
+    angular.module('math.search-raiz', []).factory('searchRaiz', function($q) {
+        return {
+            options: function(input, sub_module) {
+                var deferred = $q.defer();
+                var html = {
+                    resolve: "/search_raiz/view_search_raiz.html"
+                        //graphics: "/mi_nuevo_modulo/view_graphics.html" //Opcional
+                };
+
+                input = parse_input(input);
+
+                if (sub_module == "Punto fijo") {
+                    deferred.resolve([point_fixed(input), html]);
+                } else if (sub_module == "Bisección") {
+                    deferred.resolve([bisection(input), html]);
+                } else if (sub_module == "Newton Raphson") {
+                    deferred.resolve([newton(input), html]);
+                } else if (sub_module == "Regla falsa") {
+                    deferred.resolve([rule_false(input), html]);
+                } else {
+                    deferred.reject("Método desconocido");
+                }
+                return deferred.promise;
+            }
+        };
+
+        function point_fixed(input) {
+
+        }
+
+        function bisection(input) {
+            console.log(replaceValues(input.funcion, input.x2));
+            console.log(replaceValues(input.funcion, input.x1));
+            var fx1b = replaceValues(input.funcion, input.x1);
+            var fx2b = replaceValues(input.funcion, input.x2);
+            if (fx1b < 0 && fx2b > 0 || fx1b>0&&fx2b<0) {
+                var xr = input.x1,
+                    fx1, fxr, err;
+                var x_ant = 0;
+                for (var i = 0; i < input.iteracion; i++) {
+                    x_ant = xr;
+                    xr = (parseFloat(input.x1) + parseFloat(input.x2)) / 2;
+                    fx1 = replaceValues(input.funcion, parseFloat(input.x1));
+                    fxr = replaceValues(input.funcion, xr);
+                    err = Math.abs((xr - x_ant) / xr) * 100;
+                    if (fx1 * fxr < 0) {
+                        input.x2 = xr;
+                    } else {
+                        input.x1 = xr;
+                    }
+                }
+                return { XR: xr, error: err };
+
+            }else{
+                return {XR: "no valido",error: "100%"}
+            }
+
+        }
+
+        function newton(input) {
+            //return metodo_libreria(input);
+        }
+
+        function rule_false(input) {
+            var xr = input.x2,
+                fx1, fx2, fxr, err;
+            var x_ant = 0;
+            for (var i = 0; i < input.iteracion; i++) {
+                x_ant = xr;
+                fx1 = replaceValues(input.funcion, parseFloat(x1));
+                fx2 = replaceValues(input.funcion, parseFloat(x2));
+                xr = (parseFloat(x2) - ((fx2 * (parseFloat(x1) - parseFloat(x2))) / (fx1 - fx2)));
+                err = Math.abs((xr - x_ant) / xr) * 100;
+                fxr = replaceValues(input.funcion, xr);
+                if (fx1 * fxr < 0) {
+                    x2 = xr;
+                } else {
+                    x1 = xr;
+                }
+            }
+
+            return { XR: xr, error: err };
+        }
+
+        //METODOS PROPIOS
+        function parse_input(input) {
+            var arr = input.split(";");
+            return {
+                funcion: arr[0],
+                x1: parseInt(arr[1]),
+                x2: parseInt(arr[2]),
+                iteracion: parseInt(arr[3])
+            };
+        }
+
+        function replaceValues(funcion, x) {
+            funcion = replaceOthers(funcion, "pow");
+            funcion = replaceOthers(funcion, "x", x);
+            return eval(funcion);
+        }
+
+        function replaceAll(str, find, replace) {
+            return str.replace(new RegExp(find, 'g'), replace);
+        }
+
+        function replaceOthers(funcion, type, x) {
+            funcion = replaceAll(funcion, " ", "");
+
+            if (type == "pow") {
+                while (funcion.indexOf("^") != -1) {
+                    funcion = funcion.replace("^", "**");
+                }
+            } else if (type == "x") {
+                var pos = funcion.indexOf("x");
+                while (pos != -1) {
+                    var num_antes = (funcion[pos - 1] !== undefined && isNumeric(funcion[pos - 1]));
+                    var num_despues = (funcion[pos + 1] !== undefined && isNumeric(funcion[pos + 1]));
+
+                    if (num_antes && num_despues) {
+                        funcion = funcion.replace("x", "*" + x + "*");
+                    } else if (num_antes) {
+                        funcion = funcion.replace("x", "*" + x);
+                    } else if (num_despues) {
+                        funcion = funcion.replace("x", x + "*");
+                    } else {
+                        funcion = funcion.replace("x", x);
+                    }
+
+                    pos = funcion.indexOf("x");
+                }
+            }
+
+            return funcion;
+        }
+
+        function isNumeric(input) {
+            return (input - 0) == input && ('' + input).trim().length > 0;
+        }
+    });
+})();
+
+(function() {
+    'use strict';
+    angular.module('math.general', [])
+        .factory('general', ['$q', function($q) {
+
+            return {
+                options: function(input, sub_module) {
+                    var deferred = $q.defer();
+                    var html = {
+                        resolve: "/general/view_general.html"
+                            //graphics: "/adjust_curve/view_graphics.html"
+                    };
+
+                    if (sub_module == "Operación basica") {
+                        deferred.resolve([op_basic(input), html]);
+                    } else if (sub_module == "Derivar") {
+                        deferred.resolve([derivar(input), html]);
+                    } else {
+                        deferred.reject("Método desconocido");
+                    }
+
+                    return deferred.promise;
+                }
+            };
+
+            function op_basic(input) {
+                var solucion = eval(input);
+                return solucion;
+            }
+
+            function derivar(input) {
+                var solution;
+                try {
+                    solution = deriveExpression(input);
+                } catch (exception) {
+                    solution = "Expresiones invalidas.";
+                }
+                return solution;
+            }
+        }]);
 })();
 
 (function() {
@@ -210,182 +396,6 @@
                 return solveMinumSquare;
             }
         }]);
-})();
-
-(function() {
-    'use strict';
-    angular.module('math.general', [])
-        .factory('general', ['$q', function($q) {
-
-            return {
-                options: function(input, sub_module) {
-                    var deferred = $q.defer();
-                    var html = {
-                        resolve: "/general/view_general.html"
-                            //graphics: "/adjust_curve/view_graphics.html"
-                    };
-
-                    if (sub_module == "Operación basica") {
-                        deferred.resolve([op_basic(input), html]);
-                    } else if (sub_module == "Derivar") {
-                        deferred.resolve([derivar(input), html]);
-                    } else {
-                        deferred.reject("Método desconocido");
-                    }
-
-                    return deferred.promise;
-                }
-            };
-
-            function op_basic(input) {
-                var solucion = eval(input);
-                return solucion;
-            }
-
-            function derivar(input) {
-                var solution;
-                try {
-                    solution = deriveExpression(input);
-                } catch (exception) {
-                    solution = "Expresiones invalidas.";
-                }
-                return solution;
-            }
-        }]);
-})();
-
-(function() {
-    'use strict';
-    angular.module('math.search-raiz', []).factory('searchRaiz', function($q) {
-        return {
-            options: function(input, sub_module) {
-                var deferred = $q.defer();
-                var html = {
-                    resolve: "/search_raiz/view_search_raiz.html"
-                        //graphics: "/mi_nuevo_modulo/view_graphics.html" //Opcional
-                };
-
-                input = parse_input(input);
-
-                if (sub_module == "Punto fijo") {
-                    deferred.resolve([point_fixed(input), html]);
-                } else if (sub_module == "Bisección") {
-                    deferred.resolve([bisection(input), html]);
-                } else if (sub_module == "Newton Raphson") {
-                    deferred.resolve([newton(input), html]);
-                } else if (sub_module == "Regla falsa") {
-                    deferred.resolve([rule_false(input), html]);
-                } else {
-                    deferred.reject("Método desconocido");
-                }
-                return deferred.promise;
-            }
-        };
-
-        function point_fixed(input) {
-
-        }
-
-        function bisection(input) {
-            console.log(input);
-            var xr = input.x1,
-                fx1, fxr, err;
-            var x_ant = 0;
-            for (var i = 0; i < input.iteracion; i++) {
-                x_ant = xr;
-                xr = (parseFloat(input.x1) + parseFloat(input.x2)) / 2;
-                fx1 = replaceValues(input.funcion, parseFloat(input.x1));
-                fxr = replaceValues(input.funcion, xr);
-                err = Math.abs((xr - x_ant) / xr) * 100;
-                if (fx1 * fxr < 0) {
-                    input.x2 = xr;
-                } else {
-                    input.x1 = xr;
-                }
-            }
-            return { XR: xr, error: err };
-        }
-
-        function newton(input) {
-            //return metodo_libreria(input);
-        }
-
-        function rule_false(input) {
-            var xr = input.x2,
-                fx1, fx2, fxr, err;
-            var x_ant = 0;
-            for (var i = 0; i < input.iteracion; i++) {
-                x_ant = xr;
-                fx1 = replaceValues(input.funcion, parseFloat(x1));
-                fx2 = replaceValues(input.funcion, parseFloat(x2));
-                xr = (parseFloat(x2) - ((fx2 * (parseFloat(x1) - parseFloat(x2))) / (fx1 - fx2)));
-                err = Math.abs((xr - x_ant) / xr) * 100;
-                fxr = replaceValues(input.funcion, xr);
-                if (fx1 * fxr < 0) {
-                    x2 = xr;
-                } else {
-                    x1 = xr;
-                }
-            }
-
-            return { XR: xr, error: err };
-        }
-
-        //METODOS PROPIOS
-        function parse_input(input) {
-            var arr = input.split(";");
-            return {
-                funcion: arr[0],
-                x1: parseInt(arr[1]),
-                x2: parseInt(arr[2]),
-                iteracion: parseInt(arr[3])
-            };
-        }
-
-        function replaceValues(funcion, x) {
-            funcion = replaceOthers(funcion, "pow");
-            funcion = replaceOthers(funcion, "x", x);
-            return eval(funcion);
-        }
-
-        function replaceAll(str, find, replace) {
-            return str.replace(new RegExp(find, 'g'), replace);
-        }
-
-        function replaceOthers(funcion, type, x) {
-            funcion = replaceAll(funcion, " ", "");
-
-            if (type == "pow") {
-                while (funcion.indexOf("^") != -1) {
-                    funcion = funcion.replace("^", "**");
-                }
-            } else if (type == "x") {
-                var pos = funcion.indexOf("x");
-                while (pos != -1) {
-                    var num_antes = (funcion[pos - 1] !== undefined && isNumeric(funcion[pos - 1]));
-                    var num_despues = (funcion[pos + 1] !== undefined && isNumeric(funcion[pos + 1]));
-
-                    if (num_antes && num_despues) {
-                        funcion = funcion.replace("x", "*" + x + "*");
-                    } else if (num_antes) {
-                        funcion = funcion.replace("x", "*" + x);
-                    } else if (num_despues) {
-                        funcion = funcion.replace("x", x + "*");
-                    } else {
-                        funcion = funcion.replace("x", x);
-                    }
-
-                    pos = funcion.indexOf("x");
-                }
-            }
-
-            return funcion;
-        }
-
-        function isNumeric(input) {
-            return (input - 0) == input && ('' + input).trim().length > 0;
-        }
-    });
 })();
 
 /*
